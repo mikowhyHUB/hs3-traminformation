7dd500f104c9b88d92a106d910b1b768541c69c6
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2022-11-18 11:00:36 +0100

    Working program. Need to add more features

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import requests
 import json
 from datetime import datetime
+from tabulate import tabulate
 '''
 1. ściągnąć dane przez api
 2. potrzebujemy trzy informacje wyświetlane na tablicy: tramwaj, w jaką stronę jedzie, szacowany czas przyjazdu
 - czas przyjazdu będzie trzeba edytowac. Po pierwsze spieszy się o godzinę, po drugie chccemy by na tablicy wyswietlało nam się dane ile zostało do odjechania tramwaju
 - czas przyjazdu będzie trzbea najprawdopodobniej odjąć naszą godz
 - gdy czas przyjazdu wynosić bedzie <1, wtedy jakąś ikonkę printować zamiast czasu
+- dodać dealy
 3. Jako, ze interesują nas tramwaje w obydwie storny, musimy połączyć dwa url
 4. Interesuje nas tylko np. 5 następnych tramwajów
 5. Znalźć sposób na samouruchamianie się terminala. Chodzi o to, by się na wyświetlaczu sam wywoływał i aktualizował'''
@@ -15,22 +17,25 @@ url = requests.get(
     'https://ckan2.multimediagdansk.pl/departures?stopId=2030')
 data = json.loads(url.text)
 
+
+def converte_hrs_to_sec(item):
+    return ((item[0] * 60) + item[1]) * 60 + item[2]
+
+
+# to pewnie zrobi się jako funkcja
 lst = [i['estimatedTime'][11:19].split(':') for i in data['departures']]
-# lst2 = [i[11:19].split(':') for i in lst]
-for i in lst:
-    for j in i:
-        print(j, end=',')
+lst = [[int(i) for i in lst] for lst in lst]
+lst = [converte_hrs_to_sec(lst[i]) for i in range(0, len(lst))]
 
-# for i in data['departures']:
-#     print(i['estimatedTime'])
-'''
-jest ok:
-now = datetime.now().time() 
-test = ((now.hour * 60) - 60 + now.minute) * 60 + now.second
-'''
+current_time = datetime.now().time()
+converted_time = ((current_time.hour * 60) - 60 +
+                  current_time.minute) * 60 + current_time.second
 
-# current_time = now.strftime("%H:%M:%S")
-# current_time.
-# test = current_time.split(':')
-# test = [int(i)for i in test]
-# for i in range(3):
+lst = [round((i - converted_time)/60) for i in lst]
+
+# for i, j in zip(data['departures'], lst):
+#     # print(i['routeId'], i['headsign'] + '\t', j, sep='\t')
+#
+
+x = [(i['routeId'], i['headsign'], j) for i, j in zip(data['departures'], lst)]
+print(tabulate(x))
