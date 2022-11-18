47dcc856b6ab13d7aedc3fcfc7f69891d040742f
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2022-11-18 16:59:41 +0100

    delay has an impact on estimated time

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from tabulate import tabulate
 - czas przyjazdu będzie trzbea najprawdopodobniej odjąć naszą godz
 - gdy czas przyjazdu wynosić bedzie <1, wtedy jakąś ikonkę printować zamiast czasu
 - dodać dealy
+- znalezc time z godzina wczesniej by mozna uzyc drugi raz tej samej funkcji
 3. Jako, ze interesują nas tramwaje w obydwie storny, musimy połączyć dwa url
 4. Interesuje nas tylko np. 5 następnych tramwajów
 5. Znalźć sposób na samouruchamianie się terminala. Chodzi o to, by się na wyświetlaczu sam wywoływał i aktualizował'''
@@ -22,10 +23,41 @@ def converte_hrs_to_sec(item):
     return ((item[0] * 60) + item[1]) * 60 + item[2]
 
 
-# to pewnie zrobi się jako funkcja
+# def delay_tramp(data):
+#     test = [i['delayInSeconds'] for i in data['departures']]
+#     print(test)
+#     y = []
+#     for i in test:
+#         if i != None:
+#             if i < 0:
+#                 y.append(abs(i))
+#             else:
+#                 y.append(-abs(i))
+#     return (y)
+
+
+# print(delay_tramp(data))
+
+
 lst = [i['estimatedTime'][11:19].split(':') for i in data['departures']]
 lst = [[int(i) for i in lst] for lst in lst]
 lst = [converte_hrs_to_sec(lst[i]) for i in range(0, len(lst))]
+# converted arrival estimated time list in seconds
+print('bez delay: ', lst)
+test = [i['delayInSeconds'] for i in data['departures']]
+print('lista dealy: ', test)
+y = []
+for i, j in zip(test, lst):
+    if i != None:
+        if i < 0:
+            y.append(j + i)
+        else:
+            y.append(j - i)
+    else:
+        y.append(j)
+
+print('lista po delay: ', y)
+
 
 current_time = datetime.now().time()
 converted_time = ((current_time.hour * 60) - 60 +
@@ -33,9 +65,14 @@ converted_time = ((current_time.hour * 60) - 60 +
 
 lst = [round((i - converted_time)/60) for i in lst]
 
-# for i, j in zip(data['departures'], lst):
-#     # print(i['routeId'], i['headsign'] + '\t', j, sep='\t')
-#
 
-x = [(i['routeId'], i['headsign'], j) for i, j in zip(data['departures'], lst)]
-print(tabulate(x))
+# x = [(i['routeId'], i['headsign'], j)
+#      for i, j in zip(data['departures'], lst)]
+# # dodać listę x do y(zajezdnia 01)
+# print(tabulate(x))
+
+# lst2 = [round((i - converted_time)/60) for i in y]
+# z = [(i['routeId'], i['headsign'], j)
+#      for i, j in zip(data['departures'], lst2)]
+# # dodać listę x do y(zajezdnia 01)
+# print(tabulate(z))
